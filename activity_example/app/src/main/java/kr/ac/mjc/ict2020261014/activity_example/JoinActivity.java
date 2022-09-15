61f56bce1450fd7992798dc957cf440a7346949d
Minsang Yoo <minsang105@naver.com>
2022-09-15 15:42:14 +0900

    Update Project

--- activity_example/app/src/main/java/kr/ac/mjc/ict2020261014/activity_example/JoinActivity.java
+++ activity_example/app/src/main/java/kr/ac/mjc/ict2020261014/activity_example/JoinActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.mjc.ict2020261014.activity_example;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,17 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        Intent intent =getIntent();
+        String email = intent.getStringExtra("email");
+        String password = intent.getStringExtra("password");
+
+        EditText emailEt = findViewById(R.id.email_et);
+        emailEt.setText(email);
+
+        EditText passwordEt = findViewById(R.id.password_et);
+        passwordEt.setText(password);
+
+
     }
 }
--- activity_example/app/src/main/java/kr/ac/mjc/ict2020261014/activity_example/MainActivity.java
+++ activity_example/app/src/main/java/kr/ac/mjc/ict2020261014/activity_example/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity","onCreate");
 
        EditText emailEt = findViewById(R.id.email_et);
        EditText passwordEt = findViewById(R.id.password_et);
@@ -23,10 +25,50 @@ public class MainActivity extends AppCompatActivity {
        signupBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               String email = emailEt.getText().toString();
+               String password = passwordEt.getText().toString();
+
                Intent intent= new Intent(MainActivity.this, JoinActivity.class);
+               intent.putExtra("email",email);
+               intent.putExtra("password",password);
                startActivity(intent);
            }
        });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "onRestart");
     }
 }
