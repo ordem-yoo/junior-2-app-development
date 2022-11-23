001a11b7ac53bb78363c01050d275a5f37ff718c
Minsang Yoo <minsang105@naver.com>
2022-11-23 12:07:32 +0900

    rental app

--- rentalApp/app/src/main/java/kr/ac/mjc/rentalapp/MainActivity.java
+++ rentalApp/app/src/main/java/kr/ac/mjc/rentalapp/MainActivity.java
@@ -1,8 +1,21 @@
 package kr.ac.mjc.rentalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        completeLogin();
+
+        EditText emailEt = findViewById(R.id.email_et);
+        EditText passwordEt = findViewById(R.id.password_et);
+
+        Button signupBtn = findViewById(R.id.signup_btn);
+        Button signinBtn = findViewById(R.id.signin_btn);
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEt.getText().toString();
+                String password = passwordEt.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                completeLogin();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void completeLogin() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(this, GooglemapActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
